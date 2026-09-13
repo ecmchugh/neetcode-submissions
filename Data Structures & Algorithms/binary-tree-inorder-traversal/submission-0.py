d96cdb4e34836df0250361a56f732030c573bb29
Ethan McHugh <ethan.c.mchugh.28@dartmouth.edu>
2026-09-13 15:07:20 -0500

    Add: binary-tree-inorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-0.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+
+        def recurse(root):
+            if not root:
+                return
+            recurse(root.left)
+            ans.append(root.val)
+            recurse(root.right)
+        recurse(root)
+        return ans
